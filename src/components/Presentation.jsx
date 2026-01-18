03e3f2e6a1ce06a76725f08983a34add8efb5182
Kevan Gautier <kevangautier@gmail.com>
2026-01-18 13:20:47 +0100

    modification du css

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -2,11 +2,27 @@ function Presentation() {
     return <div className="presentation">
         <h1 className="title"> Mon parcours  </h1>
 
-        <p className="text-content">Fort d'une grande expérience dans différents domaines, j'ai travaillé dans beaucoup de métiers différents avant de réellement trouver ma voie </p>
-        <p className="text-content">J'ai autant travaillé dans le public que dans le privé et j'y ai appris une devise importante "le travail est un plaisir"</p>
-        <p className="text-content">Devenu père en 2024 j'ai décidé de changer de vie professionnelle par la même occasion et de me lancer dans le monde du dev </p>
-        <p className="text-content">Je suis aujourd'hui en formation à la Wild Code School en bootcamp pour devenir développeur full stack</p>
-    </div>
+        <section className="presentation-content">
+
+            <div className="presentation-card">
+                <span className="card-icon">📍</span>
+                <p>Fort d'une grande expérience dans différents domaines, j'ai travaillé dans beaucoup de métiers différents avant de réellement trouver ma voie </p>
+            </div>
+            <div className="presentation-card">
+                <span className="card-icon"> 💼 </span>
+                <p>J'ai autant travaillé dans le public que dans le privé et j'y ai appris une devise importante "le travail est un plaisir"</p>
+            </div>
+            <div className="presentation-card">
+                <span className="card-icon">🔄</span>
+                <p>Devenu père en 2024 j'ai décidé de changer de vie professionnelle par la même occasion et de me lancer dans le monde du dev </p>
+            </div>
+            <div className="presentation-card">
+                <span className="card-icon">🎓</span>
+                <p>Je suis aujourd'hui en formation à la Wild Code School en bootcamp pour devenir développeur full stack</p>
+
+            </div>
+        </section>
+    </div >
 }
 
 export default Presentation 
